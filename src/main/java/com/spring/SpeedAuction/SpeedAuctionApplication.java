b28c45af7b79596ed5788a1836bbc7c4c9617e81
andronisdre <andrebulow13@gmail.com>
2024-02-13 15:10:51 +0100

    initial commit, added downloaded spring boot project, added all dependencies to the pom.xml file

---
+++ src/main/java/com/spring/SpeedAuction/SpeedAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.spring.SpeedAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedAuctionApplication.class, args);
+	}
+
+}
