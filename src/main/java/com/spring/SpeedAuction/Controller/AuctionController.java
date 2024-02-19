eaa0be8d74d760a8c32e3a0b42caf877e01aafe6
andronisdre <andrebulow13@gmail.com>
2024-02-19 20:42:27 +0100

    finished auctionController, trying to fix database connection.

--- src/main/java/com/spring/SpeedAuction/Controller/AuctionController.java
+++ src/main/java/com/spring/SpeedAuction/Controller/AuctionController.java
@@ -1,5 +1,47 @@
 package com.spring.SpeedAuction.Controller;
 
+import com.spring.SpeedAuction.Models.AuctionModels;
+import com.spring.SpeedAuction.Services.AuctionServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/api/auctions")
 public class AuctionController {
+    @Autowired
+    AuctionServices auctionServices;
+
+
+    //POST
+    @PostMapping()
+    public AuctionModels createAuctionModels(@RequestBody AuctionModels auctionModels) {
+        return auctionServices.createAuctionModels(auctionModels);
+    }
+
+    //GET ALL
+    @GetMapping("/all")
+    public List<AuctionModels> getAllAuctionModels() {
+        return auctionServices.getAllAuctionModels();
+    }
+
+    //PUT by id
+    @PutMapping(value = "/{id}")
+    public AuctionModels updateAuctionModels(@RequestBody AuctionModels auctionModels) {
+        return auctionServices.updateAuctionModels(auctionModels);
+    }
+
+    //GET by id
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public AuctionModels getAuctionModelsById(@PathVariable String id) {
+        return auctionServices.getAuctionModelsById(id);
+    }
+
+    //DELETE by id
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deleteAuctionModels(@PathVariable String id) {
+        return auctionServices.deleteAuctionModels(id);
+    }
 }
 
