e77c9eb83196acfa46796902785d2bf25b9ca7f5
Rasti G <rasti.gulli1@hotmail.com>
2024-02-26 21:51:20 +0100

    Added Bids Model for SpeedAuction

---
+++ src/main/java/com/spring/SpeedAuction/Controller/BidsModelsController.java
@@ -0,0 +1,46 @@
+package com.spring.SpeedAuction.Controller;
+
+import com.spring.SpeedAuction.Models.BidsModels;
+import com.spring.SpeedAuction.Services.BidsModelsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping(value = "api/bids")
+public class BidsModelsController {
+    @Autowired
+    BidsModelsService bidsModelsService;
+
+    //POST Lägg till ett nytt bid
+    @PostMapping
+    public BidsModels createBidModel(@RequestBody BidsModels bidsModel) {
+        return bidsModelsService.createBidModels(bidsModel);
+    }
+
+    //GET /bids - Hämta en lista över alla recept.
+    @GetMapping("/all")
+    public List<BidsModels> getAllBidsModels() {
+        return bidsModelsService.getAllBidsModel();
+    }
+
+    //GET /bids/{id} - Hämta ett specifikt recept baserat på id.
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public BidsModels getAllBidsModels(@PathVariable String id) {
+        return bidsModelsService.getBidsModel(id);
+    }
+
+    //PUT /bids/{id} - Uppdatera ett specifikt recept baserat på id.
+    @PutMapping("/{id}")
+    public BidsModels updateBidModel(@PathVariable String id, @RequestBody BidsModels bidsModels) {
+        return bidsModelsService.updateBidModels(id, bidsModels);
+    }
+
+    //DELETE /bids/{id} - Ta bort ett recept baserat på id.
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deleteBidModel(@PathVariable String id){
+         return bidsModelsService.deleteBidmodels(id);
+
+    }
+
+}
---
+++ src/main/java/com/spring/SpeedAuction/Services/BidsModelsService.java
@@ -0,0 +1,82 @@
+package com.spring.SpeedAuction.Services;
+
+import com.spring.SpeedAuction.Models.BidsModels;
+import com.spring.SpeedAuction.Repository.AuctionRepository;
+import com.spring.SpeedAuction.Repository.BidsModelsRepository;
+import com.spring.SpeedAuction.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class BidsModelsService {
+    //logic
+    @Autowired
+    BidsModelsRepository bidsModelsRepository;
+    AuctionRepository auctionRepository;
+    UserRepository userRepository;
+
+    // Alla PostMan Funktioner
+
+    // GET
+    public List<BidsModels> getAllBidsModel() {
+        return bidsModelsRepository.findAll();
+    }
+
+    public BidsModels getBidsModel(String id) {
+        return bidsModelsRepository.findById(id).get();
+    }
+
+
+
+    // POST
+    public BidsModels createBidModels(BidsModels bidsModel) {
+        // Kontroll om Auction ID finns
+
+        // Kontroll om User ID Finns
+
+        return bidsModelsRepository.save(bidsModel);
+    }
+
+    // PUT
+    public BidsModels updateBidModels(String id, BidsModels updatedBidModel) {
+        BidsModels existingBidsModel = bidsModelsRepository.findById(id).get();
+
+        // Kontroll om Auction ID finns
+
+        // Kontroll om User ID Finns
+
+        //update field
+        if (updatedBidModel.getAuction_id() != null) {
+            existingBidsModel.setAuction_id(updatedBidModel.getAuction_id());
+        }
+        if (updatedBidModel.getUser_id() != null) {
+            existingBidsModel.setUser_id(updatedBidModel.getUser_id());
+        }
+        if (updatedBidModel.getAmount() != null){
+            existingBidsModel.setAmount(updatedBidModel.getAmount());
+        }
+        if (updatedBidModel.getTime() != null){
+            existingBidsModel.setTime(updatedBidModel.getTime());
+        }
+        if (updatedBidModel.getPriority() != null){
+            existingBidsModel.setPriority(updatedBidModel.getPriority());
+        }
+        return bidsModelsRepository.save(existingBidsModel);
+    }
+    // DELETE
+
+    public String deleteBidmodels(String id){
+        bidsModelsRepository.deleteById(id);
+        return "Bid deleted";
+    }
+
+}
+
+
+
+
+
