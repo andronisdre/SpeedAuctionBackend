56fe710fefc7e6d0df430f192d4f7c233cf87c61
Kassem2000 <144707158+Kassem2000@users.noreply.github.com>
2024-02-28 15:29:33 +0100

    Basic reviewModels and repository is done

--- src/main/java/com/spring/SpeedAuction/Models/ReviewModels.java
+++ src/main/java/com/spring/SpeedAuction/Models/ReviewModels.java
@@ -1,4 +1,64 @@
 package com.spring.SpeedAuction.Models;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "Review")
 public class ReviewModels {
+
+    @Id
+    private String id;
+
+    private String reviewContent;
+
+    private int rating;
+
+    @CreatedDate
+    private Date created_at;
+
+
+    public ReviewModels() {
+    }
+
+    public ReviewModels(String id, String reviewContent, int rating, Date created_at) {
+        this.id = id;
+        this.reviewContent = reviewContent;
+        this.rating = rating;
+        this.created_at = created_at;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getReviewContent() {
+        return reviewContent;
+    }
+
+    public void setReviewContent(String reviewContent) {
+        this.reviewContent = reviewContent;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
 }
--- src/main/java/com/spring/SpeedAuction/Repository/ReviewRepository.java
+++ src/main/java/com/spring/SpeedAuction/Repository/ReviewRepository.java
@@ -1,4 +1,7 @@
 package com.spring.SpeedAuction.Repository;
 
-public interface ReviewRepository {
+import com.spring.SpeedAuction.Models.ReviewModels;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<ReviewModels, String> {
 }
