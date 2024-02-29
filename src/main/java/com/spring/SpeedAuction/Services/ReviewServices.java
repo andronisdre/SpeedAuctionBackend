03d88ef7efaeacbb5ea09c716f681e39f15ab0ca
Kassem2000 <144707158+Kassem2000@users.noreply.github.com>
2024-02-29 01:05:48 +0100

    Service file is done

--- src/main/java/com/spring/SpeedAuction/Services/ReviewServices.java
+++ src/main/java/com/spring/SpeedAuction/Services/ReviewServices.java
@@ -1,4 +1,45 @@
 package com.spring.SpeedAuction.Services;
 
+import com.spring.SpeedAuction.Models.ReviewModels;
+import com.spring.SpeedAuction.Repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReviewServices {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    public ReviewModels addReview(ReviewModels review) {  // POST att lägga till review
+        return reviewRepository.save(review);
+    }
+
+    public List<ReviewModels> getAllReviews() {  // GET hämta alla reviews
+        return reviewRepository.findAll();
+    }
+
+    public ReviewModels getReviewById(String id) {  // GET hämta review genom id
+        return reviewRepository.findById(id).get();
+    }
+
+    public ReviewModels updateReview(ReviewModels review) { // PUT updatera review
+        return reviewRepository.save(review);
+    }
+
+    public String deleteReview(String id) {  // DELETE ta bort en review
+        reviewRepository.deleteById(id);
+        return "Your review is deleted";
+    }
+
+
+
+
+
+
+
+
+
 }
