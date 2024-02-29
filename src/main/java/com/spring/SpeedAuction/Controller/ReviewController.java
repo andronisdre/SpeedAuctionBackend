1d3127c54dbabe4593e11292fbea9e47ec0d658f
Kassem2000 <144707158+Kassem2000@users.noreply.github.com>
2024-02-29 01:39:35 +0100

    Controller is finish

--- src/main/java/com/spring/SpeedAuction/Controller/ReviewController.java
+++ src/main/java/com/spring/SpeedAuction/Controller/ReviewController.java
@@ -2,10 +2,14 @@ package com.spring.SpeedAuction.Controller;
 
 import com.spring.SpeedAuction.Models.ReviewModels;
 import com.spring.SpeedAuction.Services.ReviewServices;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping(value = "/api/review")
@@ -34,15 +38,19 @@ public class ReviewController {
     }
 
     // PUT
+    @PutMapping("/{id]")
+    public ResponseEntity<?> updateReview(@PathVariable String id, @Valid @RequestBody ReviewModels reviewDetails) {
+        try {
+            ReviewModels updateReview = reviewServices.updateReview(id,reviewDetails);
+            return ResponseEntity.ok(updateReview);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
+    // DELETE
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deleteReview(@PathVariable String id) {
+        return reviewServices.deleteReview(id);
+    }
 }
