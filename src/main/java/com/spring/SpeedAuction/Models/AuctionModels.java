f8505824404750124f55fe8f91668ca7c18c6f8b
andronisdre <andrebulow13@gmail.com>
2024-09-12 17:14:14 +0200

    jag och baker har lagt till AuctionBuilder klass i AuctionModels. Ska försöka utveckla på det. Knakse med Validation och required fields i builder

--- src/main/java/com/spring/SpeedAuction/Models/AuctionModels.java
+++ src/main/java/com/spring/SpeedAuction/Models/AuctionModels.java
@@ -13,175 +13,263 @@ import java.util.List;
 
 @Document(collection = "auctions")
 public class AuctionModels {
+        @Id
+        private String id;
+        @DBRef
+        private UserModels seller;
+        private int startingPrice;
+        private boolean isActive;
+        @CreatedDate
+        private Date created_at;
+        private Date updated_at;
+        private Date endOfAuction;
+        private EBrand brand;
+        private String carModel;
+        private int yearManufactured;
+        private int milesDriven;
+        private List<EColor> color;
+        private String carPng;
+        private String regNumber;
+        private ECondition condition;
+        private String description;
 
-
-    @Id
-    private String id;
-
-    @DBRef
-    private UserModels seller;
-
-    private int startingPrice;
-
-    private boolean isActive;
-
-    @CreatedDate
-    private Date created_at;
-
-    private Date updated_at;
-
-    private Date endOfAuction;
-
-    private EBrand brand;
-
-    private String carModel;
-
-    private int yearManufactured;
-
-    private int milesDriven;
-
-    private List<EColor> color;
-
-    private String carPng;
-
-    private String regNumber;
-
-    private ECondition condition;
-
-    private String description;
-
-    public AuctionModels(String id) {
-        this.id = id;
+     public AuctionModels(AuctionBuilder builder) {
+         this.id = builder.id;
+         this.seller = builder.seller;
+         this.startingPrice = builder.startingPrice;
+         this.isActive = builder.isActive;
+         this.created_at = builder.created_at;
+         this.updated_at = builder.updated_at;
+         this.endOfAuction = builder.endOfAuction;
+         this.brand = builder.brand;
+         this.carModel = builder.carModel;
+         this.yearManufactured = builder.yearManufactured;
+         this.milesDriven = builder.milesDriven;
+         this.color = builder.color;
+         this.carPng = builder.carPng;
+         this.regNumber = builder.regNumber;
+         this.condition = builder.condition;
+         this.description = builder.description;
     }
 
     public AuctionModels() {
     }
 
-    public String getId() {
+    public static class AuctionBuilder {
+        @Id
+        private String id;
+        @DBRef
+        private UserModels seller;
+        private int startingPrice;
+        private boolean isActive;
+        @CreatedDate
+        private Date created_at;
+        private Date updated_at;
+        private Date endOfAuction;
+        private EBrand brand;
+        private String carModel;
+        private int yearManufactured;
+        private int milesDriven;
+        private List<EColor> color;
+        private String carPng;
+        private String regNumber;
+        private ECondition condition;
+        private String description;
+
+        public AuctionBuilder idAuction(String id) {
+            this.id = id;
+            return this;
+        }
+        public AuctionBuilder seller(UserModels seller) {
+            this.seller = seller;
+            return this;
+        }
+        public AuctionBuilder startingPrice(int startingPrice) {
+            this.startingPrice = startingPrice;
+            return this;
+        }
+        public AuctionBuilder isActive(boolean isActive) {
+            this.isActive = isActive;
+            return this;
+        }
+        public AuctionBuilder created(Date created_at) {
+            this.created_at = created_at;
+            return this;
+        }
+        public AuctionBuilder updated(Date updated_at) {
+            this.updated_at = updated_at;
+            return this;
+        }
+        public AuctionBuilder endOfAuction(Date endOfAuction) {
+            this.endOfAuction = endOfAuction;
+            return this;
+        }
+        public AuctionBuilder EBrand(EBrand brand) {
+            this.brand = brand;
+            return this;
+        }
+        public AuctionBuilder EColor(List<EColor> color) {
+            this.color = color;
+            return this;
+        }
+        public AuctionBuilder carModel(String carModel) {
+            this.carModel = carModel;
+            return this;
+        }
+        public AuctionBuilder yearManufactured(int yearManufactured) {
+            this.yearManufactured = yearManufactured;
+            return this;
+        }
+        public AuctionBuilder milesDriven(int milesDriven) {
+            this.milesDriven = milesDriven;
+            return this;
+        }
+        public AuctionBuilder ECondition(ECondition condition) {
+            this.condition = condition;
+            return this;
+        }
+        public AuctionBuilder carPng(String carPng) {
+            this.carPng= carPng;
+            return this;
+        }
+        public AuctionBuilder regNumber(String regNumber) {
+            this.regNumber= regNumber;
+            return this;
+        }
+        public AuctionBuilder description(String description) {
+            this.description = description;
+            return this;
+        }
+        public AuctionModels build() {
+            return new AuctionModels(this);
+        }
+    }
+
+        public String getId() {
         return id;
     }
 
-    public Date getCreated_at() {
+        public Date getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(Date created_at) {
+        public void setCreated_at(Date created_at) {
         this.created_at = created_at;
     }
 
-    public int getStartingPrice() {
+        public int getStartingPrice() {
         return startingPrice;
     }
 
-    public void setStartingPrice(int startingPrice) {
+        public void setStartingPrice(int startingPrice) {
         this.startingPrice = startingPrice;
     }
 
-    public boolean isActive() {
+        public boolean isActive() {
         return isActive;
     }
 
-    public void setActive(boolean active) {
+        public void setActive(boolean active) {
         isActive = active;
     }
 
-    public void setId(String id) {
+        public void setId(String id) {
         this.id = id;
     }
 
-    public UserModels getSeller() {
+        public UserModels getSeller() {
         return seller;
     }
 
-    public void setSeller(UserModels seller) {
+        public void setSeller(UserModels seller) {
         this.seller = seller;
     }
 
-    public Date getUpdated_at() {
+        public Date getUpdated_at() {
         return updated_at;
     }
 
-    public void setUpdated_at(Date updated_at) {
+        public void setUpdated_at(Date updated_at) {
         this.updated_at = updated_at;
     }
 
-    public Date getEndOfAuction() {
+        public Date getEndOfAuction() {
         return endOfAuction;
     }
 
-    public void setEndOfAuction(Date endOfAuction) {
+        public void setEndOfAuction(Date endOfAuction) {
         this.endOfAuction = endOfAuction;
     }
 
-    public EBrand getBrand() {
+        public EBrand getBrand() {
         return brand;
     }
 
-    public void setBrand(EBrand brand) {
+        public void setBrand(EBrand brand) {
         this.brand = brand;
     }
 
-    public String getCarModel() {
+        public String getCarModel() {
         return carModel;
     }
 
-    public void setCarModel(String carModel) {
+        public void setCarModel(String carModel) {
         this.carModel = carModel;
     }
 
-    public int getYearManufactured() {
+        public int getYearManufactured() {
         return yearManufactured;
     }
 
-    public void setYearManufactured(int yearManufactured) {
+        public void setYearManufactured(int yearManufactured) {
         this.yearManufactured = yearManufactured;
     }
 
-    public int getMilesDriven() {
+        public int getMilesDriven() {
         return milesDriven;
     }
 
-    public void setMilesDriven(int milesDriven) {
+        public void setMilesDriven(int milesDriven) {
         this.milesDriven = milesDriven;
     }
 
-    public List<EColor> getColor() {
+        public List<EColor> getColor() {
         return color;
     }
 
-    public void setColor(List<EColor> color) {
+        public void setColor(List<EColor> color) {
         this.color = color;
     }
 
-    public String getCarPng() {
+        public String getCarPng() {
         return carPng;
     }
 
-    public void setCarPng(String carPng) {
+        public void setCarPng(String carPng) {
         this.carPng = carPng;
     }
 
-    public String getRegNumber() {
+        public String getRegNumber() {
         return regNumber;
     }
 
-    public void setRegNumber(String regNumber) {
+        public void setRegNumber(String regNumber) {
         this.regNumber = regNumber;
     }
 
-    public ECondition getCondition() {
+        public ECondition getCondition() {
         return condition;
     }
 
-    public void setCondition(ECondition condition) {
+        public void setCondition(ECondition condition) {
         this.condition = condition;
     }
 
-    public String getDescription() {
+        public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+        public void setDescription(String description) {
         this.description = description;
     }
 }
