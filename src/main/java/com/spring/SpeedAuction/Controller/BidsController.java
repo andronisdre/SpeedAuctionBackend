0f56b7efb7995ca42a49797b9a8135167bb8d109
Apex1887 <144783261+Apex1887@users.noreply.github.com>
2024-09-18 17:03:34 +0200

    Uppdated

--- src/main/java/com/spring/SpeedAuction/Controller/BidsController.java
+++ src/main/java/com/spring/SpeedAuction/Controller/BidsController.java
@@ -1,7 +1,6 @@
 package com.spring.SpeedAuction.Controller;
 
 import com.spring.SpeedAuction.Models.BidsModels;
-//import com.spring.SpeedAuction.Security.Services.BidsDataService;
 import com.spring.SpeedAuction.Security.Services.BidsValidateService;
 import com.spring.SpeedAuction.dto.BidsDTO;
 import com.spring.SpeedAuction.Security.Services.BidsServices;
@@ -16,8 +15,7 @@ import java.util.List;
 public class BidsController {
     @Autowired
     BidsServices bidsService;
-    //@Autowired
-    //BidsDataService bidsDataService;
+
     @Autowired
     BidsValidateService bidsValidateService;
 
